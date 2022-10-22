ba2775c29513d56ef2760ca359172c3b6142d50b
LozanoTheStain <adslozanov@gmail.com>
2022-10-22 11:59:08 -0500

    modificaciones invisibles

---
+++ Js/store.js
@@ -0,0 +1,9 @@
+// Cart 
+
+let cartIcon = document.querySelectorById('.icon')
+let cart = document.querySelector('.cart')
+let closeCart = document.querySelectorById('#close-cart')
+
+cartIcon.addEventListener('onclick' = () => {
+    cart.setAttribute.add('active')
+})
